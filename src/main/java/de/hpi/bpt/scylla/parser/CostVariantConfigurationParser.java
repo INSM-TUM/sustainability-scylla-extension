e34a284e3ee9d48a72f2553fdbc42b3bc266b2b4
Finn K <fyndalf@users.noreply.github.com>
2022-04-28 13:29:44 +0200

    Avoid parsing of fixed cost config

--- src/main/java/de/hpi/bpt/scylla/parser/CostVariantConfigurationParser.java
+++ src/main/java/de/hpi/bpt/scylla/parser/CostVariantConfigurationParser.java
@@ -7,6 +7,7 @@ import org.jdom2.Element;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class CostVariantConfigurationParser implements IDOMParser<CostVariantConfiguration> {
     @Override
@@ -19,12 +20,16 @@ public class CostVariantConfigurationParser implements IDOMParser<CostVariantCon
         Integer count = Integer.valueOf(rootElement.getAttributeValue("count"));
 
         globalConfigurationElements.forEach(element -> {
+
+            if (!Objects.equals(element.getName(), "fixed_cost")) {
             String variantID = element.getAttributeValue("id");
             Double frequency = Double.valueOf(element.getAttributeValue("frequency"));
             costVariantMap.put(variantID, frequency);
 
             System.out.println(element.getAttributeValue("id"));
-            System.out.println(element.getAttributeValue("frequency"));});
+            System.out.println(element.getAttributeValue("frequency"));
+            }
+        });
 
         return new CostVariantConfiguration(costVariantMap, count);
     }
